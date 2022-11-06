e5f048aa5af286b422082e31e6049a5b8e55a42a
avdushin <avdushinbeaver1@gmail.com>
2022-11-06 17:06:52 +0300

    Server is here

--- distance.py
+++
@@ -1,9 +0,0 @@
-import geopy.distance
-
-dist = geopy.distance.geodesic([37.46097, 55.7754],[37.47393, 55.75401]
-)
-distance = dist.m
-distance = float('{:.1f}'.format(distance))
-
-
-print(f"{distance} меторв")
